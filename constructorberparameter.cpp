83594c3800dd9f53cceca6ec0f9902e4c5c8567c
danishanr <danishaanr@gmail.com>
2025-05-13 18:58:51 +0700

    Memanggil constructor dengan parameter melalui fungsi utama

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -12,6 +12,13 @@ class mahasiswa
 mahasiswa::mahasiswa(int nim, string nama)
 {
     cout << "Constructor Dengan Parameter Terpanggil" << endl;
-    cout << "NIM : " << nim << endl;
+    cout << "NIM  : " << nim << endl;
     cout << "Nama : " << nama << endl;
+}
+
+int main()
+{
+    // memanggil constructor dengan parameter
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
 }
